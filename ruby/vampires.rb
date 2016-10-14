f222526c069c84ffb2e44d7b50fca6d58e5ccd99
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-10-14 12:11:37 -0400

    Rel 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,28 +12,33 @@ current_year = 2016
 
 puts "Do you like garlic bread? (Y/N)"
 employee_garlic = gets.chomp
+employee_garlic.to_s == 'y' ? true : false
 
 puts "Would you like to enroll in the company's health insurance program? (Y/N)"
 employee_insurance = gets.chomp
+employee_insurance.to_s == 'y' ? true : false
 
 employee_age_checked = current_year - employee_birthyear
 
 if employee_age == employee_age_checked
 	verified_age = true
+else verified_age = false
+end
 
-elsif verified_age && employee_garlic 	 
-	status = "Probably not a vampire"	
+if (verified_age && employee_garlic)
+	status = "Probably not a vampire"
 
-elsif !verified_age && (!employee_garlic || !employee_insurance) 	 
+elsif !(verified_age && employee_garlic && employee_insurance)
+	status = "Almost certainly a vampire"
+
+elsif !(verified_age && (employee_garlic || employee_insurance))	 
 	status = "Probably a vampire"
-
-elsif !verified_age && (employee_garlic && employee_insurance)
- 	 status = "Almost certainly a vampire"
-
-elsif employee_name == "Drake Cula" 	 
+	
+#elsif employee_name == "Drake Cula" || “Tu Fang”
 	status = "Definitely a vampire"
-
+	
 else
 	status = "Results inconclusive"
 end
-p status
+
+puts status
