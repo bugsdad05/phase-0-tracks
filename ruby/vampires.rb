5b3de028f0848407e7f7102ef3d9d79e0fd547f4
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-10-14 07:40:09 -0400

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+# Onboarding Questionaire (Vampire Detection Protocols)
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you?"
+employee_age = gets.chomp
+
+puts "In what year were you born?"
+employee_birthyear = gets.chomp
+
+puts "Do you like garlic bread? (Y/N)"
+employee_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance program? (Y/N)"
+employee_insurance = gets.chomp
