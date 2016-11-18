9fb685fedbe167eeb271d7ff06d977e400db4b8e
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-11-18 13:54:35 -0500

    Adjust driver code for triggers

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,41 +1,49 @@
-#Create an "encrypt" method
-
-def encrypt
-	#get user password
-	puts "type your password (lowercase only please)"
-	secret_password = gets.chomp
-	#initialize counter
-	index = 0
-	#use .length to loop thru the string and .next! to alter the string permanenetly
-	while index < secret_password.length
-	  secret_password[index] = secret_password[index].next!
-	  index += 1
-	end
-	puts secret_password
+# define method encrypt
+def encrypt(secret_password)
+  counter = 0
+  while counter < secret_password.length
+    # special case for letter z
+    if secret_password[counter] == "z" 
+      then secret_password[counter] = "a"
+    else
+      secret_password[counter] = secret_password[counter].next!
+    end
+    counter +=1
+  end
+  return secret_password
 end
 
-encrypt
-
-#Create an "decrypt" method
-
-def decrypt
-	#get user password
-	puts "type your encrypted password (lowercase only please)"
-	secret_password = gets.chomp
-	
-	#initialize counter
-	index = 0
-
-	#use .length to loop thru the string and .next! to alter the string permanenetly
-	while index < secret_password.length
-	  local_letter = secret_password[index]
-	  local_index = "abcdefghijklmnopqrstuvwxyz".index(local_letter)
-	  new_local_index = local_index - 1
-	  decrypted_letter = "abcdefghijklmnopqrstuvwxyz"[new_local_index]
-	  decrypted_password = "#{decrypted_password}" + "#{decrypted_letter}"
-	  index += 1
-	end
-	
-	puts decrypted_password
+# define method decrypt
+def decrypt (encrypted_password)
+  
+  #initialize counter
+  counter = 0
+  decrypted_password = ""
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  #use .length to loop thru the string and .next! to alter the string permanently
+  while counter < encrypted_password.length
+    local_letter = encrypted_password[counter]
+    decrypted_letter = alphabet[alphabet.index(local_letter) -1]
+    decrypted_password += decrypted_letter
+    counter += 1
+  end
+  return decrypted_password
 end
-decrypt
+
+#driver code
+sentence = ""
+while true
+  puts "Enter E to encrypt, D to decrypt, T to test, F to end program"
+  answer = gets.chomp
+  if answer == "F" then break end
+  puts "enter a phrase."
+  sentence = gets.chomp
+  if answer == "E"
+    ns = encrypt(sentence)
+  elsif answer == "D"
+    ns = decrypt(sentence)
+  else 
+    ns = encrypt(decrypt(sentence))
+  end
+  puts ns
+end
