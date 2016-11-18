b40720483b345ef795e3f0411e36fe3c5461be7c
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-11-18 16:26:52 -0500

    Add upcase letters, remarks

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -16,11 +16,11 @@ end
 # define method decrypt
 def decrypt (encrypted_password)
   
-  #initialize counter
+  #initialize counter and 2 variables
   counter = 0
   decrypted_password = ""
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  #use .length to loop thru the string and .next! to alter the string permanently
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  #use .length to loop thru the string and 'alphabet' to pull desired letter. Breaks with no alphabetic characters.
   while counter < encrypted_password.length
     local_letter = encrypted_password[counter]
     decrypted_letter = alphabet[alphabet.index(local_letter) -1]
@@ -32,16 +32,21 @@ end
 
 #driver code
 sentence = ""
+#determine to encrypt, decrypt, both, or end
 while true
-  puts "Enter E to encrypt, D to decrypt, T to test, F to end program"
-  answer = gets.chomp
+  puts "Enter 'E' to encrypt, 'D' to decrypt, 'T' to test, 'F' to end program"
+  answer = gets.chomp.upcase!
+  #end
   if answer == "F" then break end
-  puts "enter a phrase."
+  puts "Enter a phrase. (letters and spaces only, either case)"
   sentence = gets.chomp
-  if answer == "E"
+  #encrypt
+ 	if answer == "E"
     ns = encrypt(sentence)
+   #decrypt
   elsif answer == "D"
     ns = decrypt(sentence)
+  #both
   else 
     ns = encrypt(decrypt(sentence))
   end
