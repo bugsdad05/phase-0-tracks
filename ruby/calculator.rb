133f4c6fc0d5053d993f3efa1c643d9fbba5423b
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-11-23 14:34:23 -0500

    Add UI and driver code

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -2,10 +2,24 @@ def calc(a, b, operator)
 	a.send(operator, b)
 end
 
-p calc 42, 23, :+
-p calc 16, 25, :*
-p calc 18, 9, :/
-p calc 98, 67, :-
-#def sum a,b
-# yield a,b
-#end
+#p calc 42, 23, :+
+#p calc 16, 25, :*
+#p calc 18, 9, :/
+#p calc 98, 67, :-
+
+puts "Enter a number or 'q' to end program"
+while a = gets.chomp
+  case a
+  when "q"
+    puts "Thanks for using the CalcIT!"
+    break
+  else
+  	a = a.to_i
+    puts "Enter another number"
+    b = gets.chomp.to_i
+    puts "Enter your operator (+,-,*,/)"
+    operator = gets.chomp
+    calculation = calc(a, b, operator)
+  end
+  p calculation
+end
