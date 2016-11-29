c28a5b1d0e3e1fcdef33be6c4cd34281caeaed72
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-11-29 08:19:04 -0500

    Initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,27 @@
+# Pupppy Class Definition
+
+# SPECIES --------------------
+# Canis lupus familiaris hwelp (domestic dog puppy)
+
+# CHARACTERISTICS ----------
+# Dog Friendly - (Y/N)
+
+# Kid Friendly - (Y/N)
+
+# Apartment Friendly - (Y/N)
+
+# Amaount of Shedding - (scale of 1 -10)
+
+# Drooling Potential - (scale of 1 -11)
+
+# Easy to Groom - (Y/N)
+
+# Easy to Train - (Y/N)
+
+# Energy Level - (scale of 1-10)
+
+#General Intelligence - (scale of 1-10)
+
+#Intensity - (scale of 1-10)
+
+#Good for Novice Owners - (Y/N)
