f3a98f21528eed414964fc785e8e62f853e019f3
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-12-02 03:07:09 -0500

    Add initial driver code for Puppy#fetch method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,14 @@ class Puppy
     toy
   end
 
+  # Define speak method
+
 end
+
+# Driver Code
+# Initialize a new instance of "Puppy" called 'duchess'
+duchess = Puppy.new
+toy = "bone"
+p duchess.fetch(toy)
+
 
