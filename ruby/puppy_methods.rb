94756dbf86e47dd6c555b3b470e84cd52d7d38c4
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-12-02 03:27:19 -0500

    Add 'dog_years' and 'play_dead' methods with calls

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -15,7 +15,19 @@ class Puppy
   # Define roll_over method
   def roll_over
   	puts "rolls over"
-  end	
+  end
+
+  # Define dog_years method
+  def dog_years(hy)
+  	hy = hy.to_i
+  	dy = hy * 7
+  	dy
+  end
+
+  #Define play_dead method
+  def play_dead
+  	puts "Ok, ok...I'm dead!"  	
+  end
 
 end
 
@@ -26,5 +38,7 @@ toy = "bone"
 p duchess.fetch(toy)
 p duchess.speak(5)
 p duchess.roll_over
+p duchess.dog_years(9)
+p duchess.play_dead
 
 
