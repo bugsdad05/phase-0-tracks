cee890458ecedd4abc722f09082ef02faa2c83c4
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-12-02 10:48:45 -0500

    Add 'create_list' method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,3 +1,48 @@
 #GPS 2.2
 #Paired with ?????????
 
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+
+  # define the method
+  def create_list(items_string)
+    temp_array = items_string.split(' ')
+    list = Hash.new
+    temp_array.each do |item|
+		list[item] = 1    	
+    end
+    print list
+    list
+  end
+
+ #test code
+
+ test_string = "bannana orange lettuce milk"
+ create_list (test_string)
+  # input is string, needs to be hash
+  # convert to intermediate array - " now's  the time".split(' ') - will split using spaces as deliminator, returns array
+  # iterate through array and push to new hash, assigning a default value to each key - a.each {|x| print x, " -- " }
+  
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?] Use a hash carrots=> 1, bannana => 1, milk => 2
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
