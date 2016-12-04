303be01e814396daf49359e7f4f3f90d7bf25730
Jim D. Miller <millerjd@jdmillerconsulting.com>
2016-12-04 06:13:03 -0500

    Add Rel 3- Assignment complete

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -11,3 +11,31 @@ module Shout
 		words + "!!!" + " :)"
 	end
 end
+
+
+# Rel 3
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  
+  def yell_happily(words)
+  	words + "!!!" + " :)"
+  end
+end
+
+class Adult
+	include Shout
+end
+
+class Baby
+	include Shout
+end
+
+# Driver code
+new_adult = Adult.new
+new_baby = Baby.new
+
+p new_adult.yell_happily("Yay")
+p new_baby.yell_angrily("Wahhh")
