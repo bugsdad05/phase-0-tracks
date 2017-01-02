c97d2338ea62bc5de78a034f1db09543f53806d0
Jim D. Miller <millerjd@jdmillerconsulting.com>
2017-01-02 09:10:50 -0500

    Complete Rel 0, 1, 2

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -8,9 +8,20 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# show students on the home page
+# add table for new campuses
+db.execute(<<-SQL
+  CREATE TABLE IF NOT EXISTS campuses(
+    id INTEGER PRIMARY KEY,
+    city VARCHAR(10)
+  )
+SQL
+)
+
+# show students on the home page 
+# add code to make campus available
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @campuses = db.execute("SELECT * FROM campuses")
   erb :home
 end
 
@@ -22,17 +33,36 @@ get '/students/update' do
   erb :update_student
 end
 
+get '/students/delete' do 
+  erb :delete_student
+end
+
+#dump the entire database for testing
+get '/students/dump_db' do
+  @students = db.execute("SELECT * FROM students")
+  erb :dump_db
+end
+
+
 # create new students via a form
-post '/students' do
+post '/students/new' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
 # update an existing student via a form
-post '/students' do
+post '/students/update' do
   db.execute("UPDATE students
-                SET campus = (?), age = (?)
-                WHERE name = (?)")
-                [params['name'], params['campus'], params['age'].to_i]
+                SET name = (?), campus = (?), age = (?)
+                WHERE id = (?)",
+                [params['name'], params['campus'], params['age'].to_i, params['id'].to_i]);
    redirect '/'
 end
+
+# delete a student via ID
+post '/students/delete' do
+  db.execute("DELETE FROM students WHERE id = (?)",[params['id'].to_i])
+  redirect '/'
+end
+
+# add static resources
